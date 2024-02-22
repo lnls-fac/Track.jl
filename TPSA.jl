e7b369c2a9f015e7b6ba48918b334c79a67dd31e
Vitor <vitor.souza@lnls.br>
2024-02-22 16:34:43 -0300

    saving-more

--- TPSA.jl
+++ TPSA.jl
@@ -47,11 +47,10 @@ mutable struct Tpsa{V, N, T}
     end
 
 end
-C(v, n) = ui((((v+n-1)*(n+v))>>1) + n) + 1
-first_at_order(V, order) = (order==0) ? ui(0) : _binomials[C(V, order-1)]
-last_at_order(V, order)  = (order==0) ? ui(1) : _binomials[C(V, order)]
-function calc_tpsa_params(V, N)
 
+C(v, n) = ui((((v+n-1)*(n+v))>>1) + n) + 1
+function calc_tpsa_params(V, N)
+    
     osip_val_ = et_osip(V, N, N)
     osip_ = zeros(ui, _osip_val)
     powers_ = zeros(ui, (_osip_val, V))
@@ -64,75 +63,63 @@ function calc_tpsa_params(V, N)
     end
 
     # set OSIP and powers
-    counter::ui = 0
+    counter::ui = 1
     power ::Vector{ui} = zeros(ui, V)
     power1::Vector{ui} = zeros(ui, V)
     power2::Vector{ui} = zeros(ui, V)
     for n1::ui in 0:N
-        for i1::ui in first_at_order(V,n1):last_at_order(V,n1)-1
-            println("n1 = $n1, i1 = $i1")
-            # get_power(i1, power1)
-            # n2::ui = 0
-            # while n1+n2 <= N
-            #     for i2::ui in first_at_order(n2):last_at_order(n2)-1
-            #         get_power(i2, power2)
-            #         for i::ui in 0 +1:V-1 +1
-            #             power[i] = power1[i] + power2[i]
-            #             powers_[counter][i] = power[i]
-            #         end
-            #         osip_[counter+1] = get_index(power)
-            #         counter += 1
-            #     end
-            #     n2 += 1
-            # end
+        for i1::ui in first_at_order(n1):last_at_order(n1)-1
+            get_power(i1, power1)
+            n2::ui = 0
+            while n1+n2 <= N
+                for i2::ui in first_at_order(n2):last_at_order(n2)-1
+                    get_power(i2, power2)
+                    for i::ui in 1:V
+                        power[i] = power1[i] + power2[i]
+                        powers_[counter][i] = power[i]
+                    end
+                    osip_[counter] = get_index(power)
+                    counter += 1
+                end
+                n2 += 1
+            end
         end
+    end
+
+    # auxiliary functions
+
+    first_at_order(order) = (order==0) ? ui(0) : binomials_[C(V, order-1)]
+
+    last_at_order(order)  = (order==0) ? ui(1) : binomials_[C(V, order)]
+
+    function get_power(idx_, pwr::Vector{ui})
+        idx = ui(idx_) + 1
+        while binomials_[C(V, pwr[1])] < idx
+            pwr[1] += 1
+        end
+        idx = binomials_[C(V, pwr[1])] - idx
+        for i in 2:V
+            pwr[i] = pwr[i-1]
+            while ui((pwr[i]*binomials_[C(V-i, pwr[i])]) / (V+1-i)) > (idx)
+                pwr[i] -= 1
+            end
+            idx -= (pwr[i] * binomials_[C(V-i, pwr[i])]) / (V+1-i)
+            pwr[i-1] -= pwr[i]
+        end
+    end
+
+    function get_index(power_)
+        s_i::ui = 0
+        idx::ui = 0
+        for i::ui in 0:V-2
+            s_i += power_[V-i]
+            idx += (s_i * binomials_[C(i,s_i)]) / (i+1)
+        end
+        s_i += power_[1]
+        idx = binomials_[C(V, s_i)] - idx
+        return idx - 1
     end
 
     return binomials_, powers_, osip_
-end
-V=2; N=2
 
-_osip_val = et_osip(V, N, N)
-_osip = zeros(ui, _osip_val)
-_powers = zeros(ui, (_osip_val, V))
-_binomials = zeros(ui, ((N+V+1)*(N+V+2))>>1)
-
-# set binomials
-for s::ui in 0:N+V
-    _binomials[C(s, 0)] = _binomials[C(0, s)] = 1
-    for n::ui in 1:(s-1) _binomials[C(s-n, n)] = _binomials[C((s-1)-n,n)] + _binomials[C((s-1)-(n-1), (n-1))] end
 end
-_powers
-function get_power(idx_, pwr::Vector{ui})
-    idx = ui(idx_) + 1
-    while _binomials[C(V, pwr[1])] < idx
-        pwr[1] += 1
-    end
-    idx = _binomials[C(V, pwr[1])] - idx
-    for i in 2:V
-        pwr[i] = pwr[i-1]
-        while ui((pwr[i]*_binomials[C(V-i, pwr[i])]) / (V+1-i)) > (idx)
-            pwr[i] -= 1
-        end
-        idx -= (pwr[i] * _binomials[C(V-i, pwr[i])]) / (V+1-i)
-        pwr[i-1] -= pwr[i]
-    end
-end
-pwr1 = zeros(ui, V)
-get_power(0, pwr1)
-println("$(pwr1[1]) $(pwr1[2])")
-pwr1 = zeros(ui, V)
-get_power(1, pwr1)
-println("$(pwr1[1]) $(pwr1[2])")
-pwr1 = zeros(ui, V)
-get_power(2, pwr1)
-println("$(pwr1[1]) $(pwr1[2])")
-pwr1 = zeros(ui, V)
-get_power(3, pwr1)
-println("$(pwr1[1]) $(pwr1[2])")
-pwr1 = zeros(ui, V)
-get_power(4, pwr1)
-println("$(pwr1[1]) $(pwr1[2])")
-pwr1 = zeros(ui, V)
-get_power(5, pwr1)
-println("$(pwr1[1]) $(pwr1[2])")
